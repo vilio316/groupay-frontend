87d058a5a0ef441d821409e0261129b896ec2d38
David Atem <atembarry12@gmail.com>
2026-06-04 13:34:36 +0100

    minor: added feat. for users to view their own plans

--- app/(user-facing)/plans/page.tsx
+++ app/(user-facing)/plans/page.tsx
@@ -4,8 +4,60 @@ import PlanCard from "@/app/components/PlanCard";
 import { soraClass } from "../../fonts";
 import { PlusIcon } from "@phosphor-icons/react";
 import Link from "next/link";
+import { useEffect, useState } from "react";
+import { useSession } from "@/lib/authClient";
+import { PlanDetails } from "../cluster/[id]/ClusterDetailsClient";
+
+interface PlanByUser {
+  id: string;
+  createdAt: string;
+  updatedAt: string;
+  joinedAt: string;
+  cluster: {
+    id: string;
+    accountNumber: string;
+  };
+  memberCount: number;
+}
+
+async function fetchPlan(clustId: string, planId: string) {
+  const planReq = await fetch(
+    `http://localhost:3000/clusters/${clustId}/plans/${planId}`,
+    {
+      headers: {
+        "Content-Type": "application/json",
+      },
+    },
+  );
+  const planRes = await planReq.json();
+  return planRes;
+}
 
 export default function PlansPage() {
+  const { data } = useSession();
+  const [userPlans, updateUserPlans] = useState<PlanDetails[]>([]);
+  useEffect(() => {
+    async function getUserPlans() {
+      const userPlansRequest = await fetch(
+        `http://localhost:3000/users/${data?.user.id}/plans`,
+        {
+          headers: {
+            "Content-Type": "application/json",
+          },
+        },
+      );
+      const userPlansResponse: PlanByUser[] = await userPlansRequest.json();
+      const requiredIds = userPlansResponse.map((plan) => ({
+        planId: plan.id,
+        clustId: plan.cluster.id,
+      }));
+      await Promise.all(
+        requiredIds.map(({ planId, clustId }) => fetchPlan(clustId, planId)),
+      ).then((values) => updateUserPlans(values));
+    }
+    getUserPlans();
+  }, [data]);
+
   return (
     <div className="p-4 mx-auto border border-card-border rounded-xl">
       <div className="flex md:gap-x-4 gap-x-2 my-2 items-center">
@@ -22,7 +74,11 @@ export default function PlansPage() {
           <span>New Plan</span>
         </Link>
       </div>
-      <div className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4"></div>
+      <div className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4">
+        {userPlans.map((plan) => (
+          <PlanCard planObject={plan} key={plan.id} />
+        ))}
+      </div>
     </div>
   );
 }
